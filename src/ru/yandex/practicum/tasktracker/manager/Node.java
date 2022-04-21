4b63211ef74e6fd2352e56d9aca19ad2b129a0d1
Denis Mikhaylov <dnmh@yandex.ru>
2022-04-21 19:23:43 +0700

    refactor: переопределить в классе Node методы equals и hashCode

--- src/ru/yandex/practicum/tasktracker/manager/Node.java
+++ src/ru/yandex/practicum/tasktracker/manager/Node.java
@@ -1,5 +1,7 @@
 package ru.yandex.practicum.tasktracker.manager;
 
+import java.util.Objects;
+
 public class Node<T> {
     public T data;
     public Node<T> next;
@@ -9,5 +11,25 @@ public class Node<T> {
         this.data = data;
         this.next = next;
         this.prev = prev;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Node<?> node = (Node<?>) o;
+
+        return Objects.equals(data, node.data)
+                && Objects.equals(next, node.next)
+                && Objects.equals(prev, node.prev);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = data != null ? data.hashCode() : 0;
+        hash = 31 * hash + (next != null ? next.hashCode() : 0);
+        hash = 31 * hash + (prev != null ? prev.hashCode() : 0);
+        return hash;
     }
 }
