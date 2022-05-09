0d8d51ab3d1e714d4011495a886787a42770db24
Denis Mikhaylov <dnmh@yandex.ru>
2022-05-09 17:10:04 +0700

    feat: добавить класс FileBackedTasksManager с нереализованным методом save для сохранения задач в файл

---
+++ src/ru/yandex/practicum/tasktracker/manager/FileBackedTasksManager.java
@@ -0,0 +1,64 @@
+package ru.yandex.practicum.tasktracker.manager;
+
+import ru.yandex.practicum.tasktracker.model.Epic;
+import ru.yandex.practicum.tasktracker.model.Subtask;
+import ru.yandex.practicum.tasktracker.model.Task;
+
+public class FileBackedTasksManager extends InMemoryTaskManager implements TaskManager {
+
+    public void save() {
+
+    }
+
+    @Override
+    public void removeAllTasks() {
+        super.removeAllTasks();
+        save();
+    }
+
+    @Override
+    public void removeAllEpics() {
+        super.removeAllEpics();
+        save();
+    }
+
+    @Override
+    public void removeAllSubtasks() {
+        super.removeAllSubtasks();
+        save();
+    }
+
+    @Override
+    public int addTaskOfAnyType(Task task) {
+        int id = super.addTaskOfAnyType(task);
+        save();
+        return id;
+    }
+
+    @Override
+    public boolean replaceTask(Task task) {
+        boolean isSuccessfullyReplacing = super.replaceTask(task);
+        save();
+        return isSuccessfullyReplacing;
+    }
+
+    @Override
+    public boolean replaceEpic(Epic epic) {
+        boolean isSuccessfullyReplacing = super.replaceEpic(epic);
+        save();
+        return isSuccessfullyReplacing;
+    }
+
+    @Override
+    public boolean replaceSubtask(Subtask subtask) {
+        boolean isSuccessfullyReplacing = super.replaceSubtask(subtask);
+        save();
+        return isSuccessfullyReplacing;
+    }
+
+    @Override
+    public void removeTaskOfAnyTypeById(int id) {
+        super.removeTaskOfAnyTypeById(id);
+        save();
+    }
+}
